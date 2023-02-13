b75c71e73d72b374adf677b5c66116d61d922bce
Shikitos <shitovn16@gmail.com>
2023-02-14 00:22:03 +0100

    first commit

---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+      <div>
+        <h1>Welcome to the Home page!</h1>
+        <p>This is the content of the Home page.</p>
+      </div>
+    );
+}
+  
+export default Home;
+  
