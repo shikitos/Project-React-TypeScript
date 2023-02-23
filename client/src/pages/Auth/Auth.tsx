a4fb572a4af5a0c1eee58d9319abb397cf0a00c4
Shikitos <shitovn16@gmail.com>
2023-02-23 23:39:44 +0100

    Corerct auth form. Next I change this logic and states types

--- client/src/pages/Auth/Auth.tsx
+++ client/src/pages/Auth/Auth.tsx
@@ -1,3 +1,4 @@
+import { request } from 'http';
 import React, {LegacyRef, useEffect, useRef, useState} from 'react';
 import './Auth.css';
 
@@ -6,6 +7,11 @@ type hasPasswordState = {
 	mainPassword: boolean;
 	newPassword: boolean;
 	confirmPassword: boolean;
+}
+type requestStatusState = {
+	requestRequired: boolean;
+	signInData: object;
+	signUpData: object;
 }
 
 const Auth:React.FC<Props> = (props) => {
@@ -19,13 +25,23 @@ const Auth:React.FC<Props> = (props) => {
 		newPassword: false,
 		confirmPassword: false
 	});
-	const [passwordValidation, setPasswordValidation] = useState<boolean[]>([
+	const [newPasswordValidation, setNewPasswordValidation] = useState<boolean[]>([
 		false, // hasNumber
 		false, // hasSufficientLength
 		false, // hasBigChar
+		false, // doesn't match
 		false // wasValidated
 	]);
+	const [reqStatus, setReqStatus] = useState<requestStatusState>({
+		requestRequired: false,
+		signInData: {},
+		signUpData: {}
+	})
+	let signInStatus = true;
 
+	useEffect(() => {
+		console.log(reqStatus);
+	}, [reqStatus.requestRequired]);
 
 	const togglePasswordVisibility = (e: React.MouseEvent, refItem: React.RefObject<any>) => {
 		if (refItem.current) {
@@ -33,23 +49,48 @@ const Auth:React.FC<Props> = (props) => {
 		}
 	}
 
-	const handleSubmit = (e: React.SyntheticEvent) => {
+	const handleSubmit = (e: React.SyntheticEvent, typeOfSubmit: string) => {
 		e.preventDefault();
 		const target = e.target as typeof e.target & {
+			fullName: {value: string},
+			username: {value: string},
 			email: {value: string},
 			password: {value: string},
+			confirmPassword: {value: string}
 		}
-		const email = target.email.value;
-		const password = target.password.value;
-		console.log(`Email: ${email}, password: ${password}`);
-		setPasswordValidation(previousState => [
-			/\d/.test(password),
-			password.length >= 8,
-			/[A-Z]/.test(password),
-			true
-		]);
-		const invalidValidations = passwordValidation.filter(elem => !elem);
-		if (invalidValidations.length) return false;
+		if (typeOfSubmit === 'signUp') {
+			const fullName = target.fullName.value;
+			const username = target.username.value;
+			const email = target.email.value;
+			const password = target.password.value;
+			const confirmPassword = target.confirmPassword.value;
+			if (password !== confirmPassword) {
+				setNewPasswordValidation(prev => [...prev.slice(0, 3), true]);
+				return false;
+			}
+			setNewPasswordValidation(previousState => [
+				/\d/.test(password),
+				password.length >= 8,
+				/[A-Z]/.test(password),
+				true
+			]);
+			const invalidValidations = newPasswordValidation.filter(elem => !elem);
+			if (invalidValidations.length) return false;
+			setReqStatus(prev => ({
+				...prev,
+				requestRequired: true,
+				signUpData: {
+					fullName: fullName,
+					username: username,
+					email: email,
+					password: password
+				}
+			}));
+		} else {
+			const email = target.email.value;
+			const password = target.password.value;
+
+		}
 	}
 
 	return (
@@ -63,7 +104,7 @@ const Auth:React.FC<Props> = (props) => {
 						isRegistered ?
 							<form
 								ref={formRef}
-								onSubmit={handleSubmit}
+								onSubmit={(e) => handleSubmit(e, "signIn")}
 							>
 								<div className="auth-form__div">
 									<label htmlFor="form__login">
@@ -94,13 +135,9 @@ const Auth:React.FC<Props> = (props) => {
 										}
 									</div>
 									{
-										(passwordValidation[3] && passwordValidation.some(value => !value)) &&
+										!signInStatus &&
 										<p>
-											{`Your password must contain: 
-											${passwordValidation[0] ? "" :  `at least one number${(!passwordValidation[1] || !passwordValidation[2]) && ", "}` }
-											${passwordValidation[1] ? "" :  `length more than 8 symbols${!passwordValidation[2] ? ", " : "."}` }
-											${passwordValidation[2] ? "" :  "one UPPERCASE char." }
-											`}
+											Your password or email is incorrect. Try again.
 										</p>
 									}
 								</div>
@@ -111,19 +148,19 @@ const Auth:React.FC<Props> = (props) => {
 							:
 							<form
 								ref={formRef}
-								onSubmit={handleSubmit}
+								onSubmit={(e) => handleSubmit(e, "signUp")}
 							>
 								<div className="auth-form__div">
 									<label htmlFor="form__name">
 										Full Name
 									</label>
-									<input className="form__name" id="form__name"  type="text" name="text" placeholder="Enter your full name" />
+									<input className="form__name" id="form__name"  type="text" name="fullName" placeholder="Enter your full name" />
 								</div>
 								<div className="auth-form__div">
 									<label htmlFor="form__login">
 										Username
 									</label>
-									<input className="form__login" id="form__username"  type="text" name="text" placeholder="Enter your username" />
+									<input className="form__username" id="form__username"  type="text" name="username" placeholder="Enter your username" />
 								</div>
 								<div className="auth-form__div">
 									<label htmlFor="form__login">
@@ -140,7 +177,7 @@ const Auth:React.FC<Props> = (props) => {
 											className="form__password"
 									        id="form__password"
 									        type="password"
-									        name="password"
+									        name="confirmPassword"
 									        placeholder="Enter your password"
 											ref ={newPasswordRef}
 											onChange={(e: React.ChangeEvent<HTMLInputElement>) => setHasPassword(prevState => ({ ...prevState, newPassword: !!e.target.value }))}
@@ -173,6 +210,20 @@ const Auth:React.FC<Props> = (props) => {
 											<span className="icon-eye" onClick={(e) => togglePasswordVisibility(e, confirmPasswordRef)}></span>
 										}
 									</div>
+									{
+										(newPasswordValidation[newPasswordValidation.length - 1] && newPasswordValidation.some(value => !value)) &&
+										<p>
+											{
+												newPasswordValidation[3] ?
+													'Your password wasn\'t  the same ' :
+													`Your password must contain: 
+														${newPasswordValidation[0] ? "" :  `at least one number${(!newPasswordValidation[1] || !newPasswordValidation[2]) && ", "}` }
+														${newPasswordValidation[1] ? "" :  `length more than 8 symbols${!newPasswordValidation[2] ? ", " : "."}` }
+														${newPasswordValidation[2] ? "" :  "one UPPERCASE char." }
+													`
+											}
+										</p>
+									}
 								</div>
 								<div className="auth-form__div">
 									<input className="form__submit" id="form__submit" type="submit" value="Sign In" />
